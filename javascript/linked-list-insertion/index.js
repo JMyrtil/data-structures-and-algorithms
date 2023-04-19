6d576cbadcafd026d596b80dce923b25c91f0464
Joel Myrtil <myrtil.joel@gmail.com>
2023-04-19 15:45:22 -0700

    CC12

--- javascript/linked-list-insertion/index.js
+++ javascript/linked-list-insertion/index.js
@@ -226,4 +226,37 @@ class PseudoQueue {
   }
 }
 
-module.exports = { LinkedList, Node, Queue, Stack, PseudoQueue };
+class AnimalShelter {
+  constructor() {
+    this.dogQueue = [];
+    this.catQueue = [];
+  }
+
+  enqueue(animal) {
+    if (animal.species === 'dog') {
+      this.dogQueue.push(animal);
+    } else if (animal.species === 'cat') {
+      this.catQueue.push(animal);
+    }
+  }
+
+  dequeue(pref) {
+    if (pref === 'dog') {
+      return this.dogQueue.shift() || null;
+    } else if (pref === 'cat') {
+      return this.catQueue.shift() || null;
+    } else {
+      return null;
+    }
+  }
+}
+
+class Animal {
+  constructor(species, name) {
+    this.species = species;
+    this.name = name;
+  }
+}
+
+
+module.exports = { LinkedList, Node, Queue, Stack, PseudoQueue, AnimalShelter, Animal };
