5275bb2ba9731b12839c2ac65a35996123b7b24d
Joel Myrtil <myrtil.joel@gmail.com>
2023-04-19 04:36:25 -0700

    CC10

--- javascript/linked-list-insertion/index.js
+++ javascript/linked-list-insertion/index.js
@@ -130,4 +130,78 @@ class LinkedList {
   }
 }
 
-module.exports = { LinkedList, Node };
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+  }
+
+  pop() {
+    if (!this.top) {
+      throw new Error('Stack is empty');
+    }
+    const value = this.top.value;
+    this.top = this.top.next;
+    return value;
+  }
+
+  peek() {
+    if (!this.top) {
+      throw new Error('Stack is empty');
+    }
+    return this.top.value;
+  }
+
+  isEmpty() {
+    return !this.top;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.front) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+  }
+
+  dequeue() {
+    if (!this.front) {
+      throw new Error('Queue is empty');
+    }
+    const value = this.front.value;
+    this.front = this.front.next;
+    if (!this.front) {
+      this.back = null;
+    }
+    return value;
+  }
+
+  peek() {
+    if (!this.front) {
+      throw new Error('Queue is empty');
+    }
+    return this.front.value;
+  }
+
+  isEmpty() {
+    return !this.front;
+  }
+}
+
+module.exports = { LinkedList, Node, Queue, Stack };
