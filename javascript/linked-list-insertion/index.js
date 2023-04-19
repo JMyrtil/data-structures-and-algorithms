b0f3975a5d1e4413141b3df87cb241910175c3ac
Joel Myrtil <myrtil.joel@gmail.com>
2023-04-19 04:17:40 -0700

    CC7

--- javascript/linked-list-insertion/index.js
+++ javascript/linked-list-insertion/index.js
@@ -110,6 +110,24 @@ class LinkedList {
 
     return current.value;
   }
+
+  zipLists(list1, list2) {
+    let current1 = list1.head;
+    let current2 = list2.head;
+    let newList = new LinkedList();
+
+    while (current1 || current2) {
+      if (current1) {
+        newList.append(current1.value);
+        current1 = current1.next;
+      }
+      if (current2) {
+        newList.append(current2.value);
+        current2 = current2.next;
+      }
+    }
+    return newList;
+  }
 }
 
 module.exports = { LinkedList, Node };
